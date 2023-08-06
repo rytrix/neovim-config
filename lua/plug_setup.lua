44c61470a03b284d9e72952a86f7559b5a91ad8e
ryan <ryan@debian.wowzers.abc>
2023-08-06 16:40:05 -0400

    fixed tab bug from nvim-cmp

--- lua/plug_setup.lua
+++ lua/plug_setup.lua
@@ -77,6 +77,7 @@ require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = {
 		-- Replace these with whatever servers you want to install
+		'lua_ls',
 		'rust_analyzer',
 		'clangd',
 		'wgsl_analyzer'
@@ -177,18 +178,18 @@ cmp.setup {
 			select = true,
 		},
 		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
+			if cmp.visible() == 1 then
 				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
+			elseif luasnip.expand_or_jumpable() == 1 then
 				luasnip.expand_or_jump()
 			else
 				fallback()
 			end
 		end, { 'i', 's' }),
 		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
+			if cmp.visible() == 1 then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
+			elseif luasnip.jumpable(-1) == 1 then
 				luasnip.jump(-1)
 			else
 				fallback()
