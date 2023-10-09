8e3a007ddec70c929cc07c44984e49e3cb45791d
ryan <ryan@debian.wowzers.abc>
2023-10-09 10:51:01 -0400

    added in a function to remove trailing whitespace at the end of lines when the buffer is saved

--- init.lua
+++ init.lua
@@ -11,8 +11,14 @@ vim.g.mapleader = ' '
 vim.g.localleader = ' '
 
 -- IMPORTS
-require('plug')        -- Plugins
-require('vars')        -- Variables
-require('opts')        -- Options
-require('keys')        -- Keymaps
+require('plug') -- Plugins
+require('vars') -- Variables
+require('opts') -- Options
+require('keys') -- Keymaps
 
+-- Highlight trailing whitespace in red
+-- vim.cmd('highlight ExtraWhitespace ctermbg=red guibg=red')
+-- vim.cmd('match ExtraWhitespace /\\s\\+$/')
+
+-- Automatically remove trailing whitespace before writing the buffer
+vim.cmd([[autocmd BufWritePre * %s/\s\+$//e]])
