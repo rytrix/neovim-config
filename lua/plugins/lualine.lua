f3be6cc1c6d35813c0581fb6395e4c3c8fbcf03c
Ryan <rmw071102@gmail.com>
2024-04-01 11:37:54 -0400

    It looks cool but i dont like it

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,18 +1,18 @@
 return {
-    {
-        'nvim-lualine/lualine.nvim', -- bar on the bottom of the screen
-        dependencies = {
-            'kyazdani42/nvim-web-devicons',
-            lazy = true
-        },
-        config = function()
-            require('lualine').setup {
-                options = {
-                    -- theme = 'dracula-nvim'
-                    -- theme = 'onedark'
-                    theme = 'catppuccin'
-                }
-            }
-        end
-    },
+    -- {
+    --     'nvim-lualine/lualine.nvim', -- bar on the bottom of the screen
+    --     dependencies = {
+    --         'kyazdani42/nvim-web-devicons',
+    --         lazy = true
+    --     },
+    --     config = function()
+    --         require('lualine').setup {
+    --             options = {
+    --                 -- theme = 'dracula-nvim'
+    --                 -- theme = 'onedark'
+    --                 theme = 'catppuccin'
+    --             }
+    --         }
+    --     end
+    -- },
 }
