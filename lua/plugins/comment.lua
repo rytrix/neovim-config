f3c0fa3cccac2d202f7628ce574b304f1b009990
Ryan <rmw071102@gmail.com>
2024-06-01 11:51:21 -0400

    commented out comment plugin since it is built in in neovim 0.10

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,10 +1,10 @@
 return {
-    {
-        'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
-        lazy = true,
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require('Comment').setup()
-        end
-    }
+    -- {
+    --     'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
+    --     lazy = true,
+    --     event = { "BufReadPre", "BufNewFile" },
+    --     config = function()
+    --         require('Comment').setup()
+    --     end
+    -- }
 }
