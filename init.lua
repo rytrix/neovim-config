7ad8e6e2fbb8d0e59d22f026af2035d979e04406
ryan <rmw071102@gmail.com>
2025-09-17 01:12:20 -0400

    neovim update

--- init.lua
+++ init.lua
@@ -3,19 +3,19 @@
 -- LEADER
 -- These keybindings need to be defined before the first /
 -- is called; otherwise, it will default to "\"
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
-vim.g.mapleader = ' '
-vim.g.localleader = ' '
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+vim.g.mapleader = " "
+vim.g.localleader = " "
 
 -- IMPORTS
-require('plug') -- Plugins
-require('vars') -- Variables
-require('opts') -- Options
-require('keys') -- Keymaps
+require("plug") -- Plugins
+require("vars") -- Variables
+require("opts") -- Options
+require("keys") -- Keymaps
 
 -- Highlight trailing whitespace in red
--- vim.cmd('highlight ExtraWhitespace ctermbg=red guibg=red')
--- vim.cmd('match ExtraWhitespace /\\s\\+$/')
+-- vim.cmd("highlight ExtraWhitespace ctermbg=red guibg=red")
+-- vim.cmd("match ExtraWhitespace /\\s\\+$/")
 
 -- Automatically remove trailing whitespace before writing the buffer
 -- vim.cmd([[autocmd BufWritePre * %s/\s\+$//e]])
